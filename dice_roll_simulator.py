17600f6d5240661d7a8631a6ed61c7ab86e453f0
Elisabed <elisabedisheshaberidze@gmail.com>
2026-02-22 18:28:53 +0400

    Simple dice simulator

--- dice_roll_simulator.py
+++ dice_roll_simulator.py
@@ -2,4 +2,4 @@ from random import randint
 def get_dice():
     dice = randint(1, 6)
     return dice
-print(f'The number on the dice is: {get_dice()}')
+print(f'The number on the dice is:  {get_dice()}')
